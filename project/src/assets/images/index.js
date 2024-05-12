b92e899b490a9f1c5694b66422ff17b6930bb2a3
Altynaim6 <151496795+Altynaim6@users.noreply.github.com>
2024-05-12 15:39:34 +0600

    first commit

---
+++ project/src/assets/images/index.js
@@ -0,0 +1,18 @@
+// src/assets/images/index.js
+import kyrgyzstan from './kyrgyzstan.jpeg';
+import issykKul from './issyk_kul.jpeg';
+import alaArcha from './ala_archa.jpg';
+import buranaTower from './burana_tower.jpg';
+import chuiValley from './chui_valley.jpg';
+import saryChelek from './sary_chelek.jpg';
+import altynArashan from './altyn_arashan.jpg';
+
+export {
+  kyrgyzstan,
+  issykKul,
+  alaArcha,
+  buranaTower,
+  chuiValley,
+  saryChelek,
+  altynArashan
+};
